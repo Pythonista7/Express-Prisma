83b12c7c6c7dbbabbca525943ac0cacc3ed58413
Pythonista7 <ashwin.mirskar@gmail.com>
2022-11-21 21:18:49 +0530

    minor tasks: add cors(),json(),basic route log

--- index.ts
+++ index.ts
@@ -1,10 +1,16 @@
-import express, { Request, Response, NextFunction } from "express";
+import express, { json } from "express";
 import config from "./src/configs/general.config";
+import basicLogger from "./src/middlewares/basicLogger";
 import errHandler from "./src/middlewares/handleError";
 import notFound from "./src/middlewares/notFound";
 import router from "./src/routes/router";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors());
+app.use(json());
+app.use(basicLogger);
 
 app.use(router);
 
